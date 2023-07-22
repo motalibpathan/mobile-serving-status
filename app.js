61a7d0d1a869fd061884fbb72d95489a736a6988
motalibpathan <motalibpathan11@gmail.com>
2023-07-22 18:32:42 +0600

    try to solve cors origin

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // if specified route
 app.use("/api/status/", statusRoute);
