c860146089159de0e5b5a7a7234008883cbc28d7
motalibpathan <motalibpathan11@gmail.com>
2023-07-22 18:27:01 +0600

    axios instance change

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: window.location.hostname.includes("https")
+    ? window.location.hostname.includes("https") + "/api"
+    : "http://localhost:3000/api",
 });
 
 export default axiosInstance;
